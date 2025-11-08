d8e980b2be90ef11f5ed3d1f3b0ec7960be7cfa6
齋藤将平 <shohei@shohei-MacBook.local>
2025-11-08 17:13:53 +0900

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Program Teacher",
+  description: "Program Teacher",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+
+        <header className="flex items-center bg-gray-600 text-white h-12 px-6">
+          <h1 className="text-xl font-bold">Program Teacher</h1>
+        </header>
+        <div className="flex min-h-screen">
+          <aside className="w-64 bg-gray-100 border-r p-4">
+          <nav className="space-y-2">
+            <a href="#" className="block p-2 rounded hover:bg-gray-200">
+              warning
+            </a>
+            <a href="#" className="block p-2 rounded hover:bg-gray-200">
+              
+            </a>
+          </nav> 
+          </aside>
+        </div>
+        {children}
+      </body>
+    </html>
+  );
+}
